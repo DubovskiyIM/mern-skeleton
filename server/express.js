f0fa39aa59e1f7385f88ddae218db34c1e97002e
IgnatDubovskiy <57371070+IgnatDubovskiy@users.noreply.github.com>
2020-10-02 10:53:53 +0300

    Implemented SSR, updating template to generate static html and css

--- server/express.js
+++ server/express.js
@@ -6,8 +6,17 @@ import compress from 'compression';
 import cors from 'cors';
 import helmet from 'helmet';
 
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { StaticRouter } from 'react-router';
+import MainRouter from '../client/MainRouter';
+import { ServerStyleSheets, ThemeProvider } from '@material-ui/styles';
+import theme from './../client/theme';
+
 import UserRoutes from './routes/user.routes';
 import AuthRoutes from './routes/auth.routes';
+
+import Template from '../template';
 
 import devBundle from './devBundle';
 
@@ -28,11 +37,33 @@ app
 	.use('/', AuthRoutes)
 	.use((err, req, res, next) => {
 		if (err.name === 'UnauthorizedError') {
-			res.status(401).json({ 'error': err.name + ': ' + err.message });
+			res.status(401).json({ 'error': `${err.name}: ${err.message}` });
 		} else if (err) {
-			res.status(400).json({ 'error': err.name + ': ' + err.message });
+			res.status(400).json({ 'error': `${err.name}: ${err.message}` });
 			console.log(err);
 		}
 	});
+
+app.get('*', (req, res) => {
+	const sheets = new ServerStyleSheets();
+	const context = {};
+	const markup = ReactDOMServer.renderToString(
+		sheets.collect(
+			<StaticRouter location={req.url} context={context}>
+				<ThemeProvider theme={theme}>
+					<MainRouter/>
+				</ThemeProvider>
+			</StaticRouter>
+		));
+
+	if (context.url) {
+		return res.redirect(303, context.url);
+	}
+	const css = sheets.toString();
+	res.status(200).send(Template({
+		markup: markup,
+		css: css
+	}));
+});
 
 export default app;
